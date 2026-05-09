0369094df1d0948aea1da1ac6d54dd38bce3d6fa
Breanna47 <breanna.watson@yahoo.com>
2026-05-09 18:30:12 -0400

    filter not working

--- movies.js
+++ movies.js
@@ -14,7 +14,21 @@ function handleInputKeyDown(event) {
   }
 }
 
+function renderMovies(filter) {
+  const filteredMovies = document.querySelector(".movies");
 
+  const movies = getMovies();
+
+  console.log(filter)
+  if (event.target.value === "YEAR") {
+    console.log(filter)
+  const filteredMovies = movies.sort((a, b) => b.Year - a.Year);
+console.log(filteredMovies)
+}
+else if (filter === "OLDYEAR") {
+    movies.sort((a, b) => a.Year - b.Year);
+}
+}
 
 function renderMovies(movies) {
   document.querySelector(".movies").innerHTML = movies
@@ -34,13 +48,20 @@ function renderMovies(movies) {
     .join("");
 }
 
+
+function filterMovies(event) {
+    renderMovies(event.target.value)
+  } 
+
+  
+
+  renderMovies(filteredMovies);
+
+
 async function getMovies() {
   if(searchValue.length < 3) {
     alert("Please enter at least 3 characters to search for movies.");
     return;
-  }
-  if (filter === "YEAR") {
-    movies.sort((a, b) => b.Year - a.Year);
   }
   const res = await fetch(
     `https://www.omdbapi.com/?apikey=774d6e3b&s=${searchValue}`
@@ -48,9 +69,4 @@ async function getMovies() {
   const data = await res.json();
   const movies = data.Search;
   renderMovies(movies);
-
-
-
-
-
 }
